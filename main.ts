26071400ab568d8b15c1cc226a1defa94ae95f53
Lucas-101 <35174794+Lucas-101@users.noreply.github.com>
2019-07-18 19:56:45 -0700

    Update 1.

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+function Thing() {
+    light.showRing("blue red red red red red red red red red")
+    light.showRing("red blue red red red red red red red red")
+    light.showRing("red red blue red red red red red red red")
+    light.showRing("red red red blue red red red red red red")
+    light.showRing("red red red red blue red red red red red")
+    light.showRing("red red red red red blue red red red red")
+    light.showRing("red red red red red red blue red red red")
+    light.showRing("red red red red red red red blue red red")
+    light.showRing("red red red red red red red red blue red")
+    light.showRing("red red red red red red red red red blue")
+}
+
+
 
